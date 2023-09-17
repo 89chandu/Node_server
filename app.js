71e7c39d0823efc2fca1913f78e4c16faa69c9d9
“Chandu” <Chandubopche321@gmail.com>
2023-09-17 13:37:11 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const http = require('http');
 
+const routes = require('./routes');
 
-const server = http.createServer((req,res) => {
-    console.log(req.uri,req.method,req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>my first page</title></head>');
-    res.write('<body><h1>Hello from node.js </h1></body>');
-    res.write('/html');
-    res.end();
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
-console.log("Server Connected");
