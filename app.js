4b696a77577e5eada6031f4f0310c4e805c02987
“Chandu” <Chandubopche321@gmail.com>
2023-09-17 11:07:03 +0530

    request

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http');
 
-const routes = require('./routes');
 
-console.log(routes.someText);
+const server = http.createServer((req,res) => {
+    console.log(req.uri,req.method,req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>my first page</title></head>');
+    res.write('<body><h1>Hello from node.js </h1></body>');
+    res.write('/html');
+    res.end();
+});
 
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+server.listen(4000);
+console.log("Server Connected");
